83e9283cbc0044c4965ba301ad6e2b9766cfafac
INSOMNIAvid <scone_godlier_9l@icloud.com>
2025-05-11 19:57:04 +0300

    Create constants.js

---
+++ game-project/shared/constants.js
@@ -0,0 +1,16 @@
+export const MAP_SIZE = 100;
+export const PLAYER_SPEED = 0.2;
+export const BULLET_SPEED = 1.5;
+export const PLAYER_HEALTH = 100;
+export const WEAPONS = {
+  rifle: {
+    damage: 10,
+    ammo: 30,
+    fireRate: 500
+  },
+  pistol: {
+    damage: 7,
+    ammo: 15,
+    fireRate: 300
+  }
+};
