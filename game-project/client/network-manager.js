c3ede012a0822b07afe1d6b7a9e5b1efec9fc5c0
INSOMNIAvid <scone_godlier_9l@icloud.com>
2025-05-11 20:18:06 +0300

    Update network-manager.js

--- game-project/client/network-manager.js
+++ game-project/client/network-manager.js
@@ -1,24 +1,71 @@
-class NetworkManager {
+import { SOCKET_EVENTS } from '../shared/constants.js';
+
+export default class NetworkManager {
   constructor(game) {
-    this.socket = io();
     this.game = game;
+    this.socket = io();
     this.setupEventListeners();
   }
 
   setupEventListeners() {
-    this.socket.on('gameInit', (data) => this.game.init(data));
-    this.socket.on('playerJoined', (player) => this.game.addPlayer(player));
-    this.socket.on('playerLeft', (id) => this.game.removePlayer(id));
-    // Другие сетевые события...
+    this.socket.on(SOCKET_EVENTS.GAME_INIT, (data) => {
+      this.game.onGameInit(data);
+    });
+
+    this.socket.on(SOCKET_EVENTS.PLAYER_JOINED, (player) => {
+      this.game.onPlayerJoined(player);
+    });
+
+    this.socket.on(SOCKET_EVENTS.PLAYER_LEFT, (id) => {
+      this.game.onPlayerLeft(id);
+    });
+
+    this.socket.on(SOCKET_EVENTS.PLAYER_MOVED, (data) => {
+      this.game.onPlayerMoved(data);
+    });
+
+    this.socket.on(SOCKET_EVENTS.BULLET_FIRED, (bullet) => {
+      this.game.onBulletFired(bullet);
+    });
+
+    this.socket.on(SOCKET_EVENTS.BULLET_REMOVED, (id) => {
+      this.game.onBulletRemoved(id);
+    });
+
+    this.socket.on(SOCKET_EVENTS.DOOR_TOGGLED, (data) => {
+      this.game.onDoorToggled(data);
+    });
+
+    this.socket.on(SOCKET_EVENTS.PLAYER_HEALTH_UPDATE, (data) => {
+      this.game.onPlayerHealthUpdate(data);
+    });
+
+    this.socket.on(SOCKET_EVENTS.PLAYER_DIED, (data) => {
+      this.game.onPlayerDied(data);
+    });
+
+    this.socket.on(SOCKET_EVENTS.CHAT_MESSAGE, (data) => {
+      this.game.onChatMessage(data);
+    });
   }
 
   sendPlayerMove(data) {
-    this.socket.emit('playerMove', data);
+    this.socket.emit(SOCKET_EVENTS.PLAYER_MOVED, data);
   }
 
   sendPlayerShoot(data) {
-    this.socket.emit('playerShoot', data);
+    this.socket.emit(SOCKET_EVENTS.PLAYER_SHOOT, data);
+  }
+
+  sendToggleDoor(doorIndex) {
+    this.socket.emit(SOCKET_EVENTS.DOOR_TOGGLED, doorIndex);
+  }
+
+  sendChatMessage(message) {
+    this.socket.emit(SOCKET_EVENTS.CHAT_MESSAGE, message);
+  }
+
+  sendPlayerReload() {
+    this.socket.emit(SOCKET_EVENTS.PLAYER_RELOAD);
   }
 }
-
-export default NetworkManager;
